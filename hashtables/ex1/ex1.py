bbc8c9f0093f4c50fa43609f0c2a76d10c0393d5
meep-morp <gracieaerogirl@gmail.com>
2020-09-04 10:24:33 -0500

    Finished ex 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,21 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    if length < 2:
+        return None
 
+    item_weights = {}
+    for i in range(length):
+        value = limit - weights[i]
+
+        if value in item_weights:
+            val_i = item_weights[value]
+            rtn = [i, val_i]
+            return rtn
+
+        else:
+            item_weights[weights[i]] = i
+
+    print("None found")
     return None
+
+
+print(get_indices_of_item_weights([12, 6, 7, 14, 19, 3, 0, 25, 40], 9, 7))
