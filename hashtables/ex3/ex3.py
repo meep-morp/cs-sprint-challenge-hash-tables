faac5449de4de7e7759e7ac83cf003e6f412d4d9
meep-morp <gracieaerogirl@gmail.com>
2020-09-04 11:14:26 -0500

    Finished 2, 3, 4:

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,22 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+    # Similar to word count -- count amount of times num appears and check if it matches num arrays
+    num_count = {}
+
+    # Init arrays
+    for i in arrays:
+        for j in i:
+            if j in num_count:
+                num_count[j] += 1
+            else:
+                num_count[j] = 1
+
+    # Check if appears in all arrays
+    for num in num_count:
+        if num_count[num] == len(arrays):
+            result.append(num)
+        else:
+            continue
 
     return result
 
